cf4073eca89f669d016db391a7acb57970de6acf
Jordi Gomis <jgc.jordi@gmail.com>
2019-11-21 15:17:19 +0100

    adding css to footer and app view and Phones All view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Footer.scss';
 
 
 function Footer(props) {
 
   return (
-    <div className="Footer">
-        <div>This is the Footer asdfas asd fasd fa sdf asdfas df asdf asd fasdfas df asdf asd 
-            fa sdf asd  as df asd fas df as df asd f asd f a  asd fa sdf asd f asdf asdf </div>
-    </div>
+    <footer class="Footer">
+      <div class="copyright">
+        <p class="copyright-text">Copyright &copy; 2019 Phone Bear Spain S.L.U. All Rights Reserved.</p>
+      </div>
+    </footer>
   );
 }
 export default Footer;
-  
