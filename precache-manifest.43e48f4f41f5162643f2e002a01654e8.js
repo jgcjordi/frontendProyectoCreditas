bf8f23ead7183ef87dedb29a84ac396a4c3180f4
Jordi Gomis <jgc.jordi@gmail.com>
2020-05-07 10:15:41 +0200

    Updates

---
+++ precache-manifest.43e48f4f41f5162643f2e002a01654e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617290428fbe42aa28c3a60d673b25f3",
+    "url": "/frontendProyectoCreditas/index.html"
+  },
+  {
+    "revision": "dd254c087b074468aa12",
+    "url": "/frontendProyectoCreditas/static/css/main.3cb757b0.chunk.css"
+  },
+  {
+    "revision": "60955924172837e101fb",
+    "url": "/frontendProyectoCreditas/static/js/2.65bb0a9c.chunk.js"
+  },
+  {
+    "revision": "dd254c087b074468aa12",
+    "url": "/frontendProyectoCreditas/static/js/main.ab8a3665.chunk.js"
+  },
+  {
+    "revision": "5bed26422d1bb9608647",
+    "url": "/frontendProyectoCreditas/static/js/runtime-main.b998b14b.js"
+  },
+  {
+    "revision": "b268748bcf3a68e95a452b35515bba56",
+    "url": "/frontendProyectoCreditas/static/media/phoneBear.b268748b.jpg"
+  }
+]);
